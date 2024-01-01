5315b8cab3366587674680ccb061bcd1645ac40b
Leuel-Tesfaye <131950742+Leuel-Tesfaye@users.noreply.github.com>
2024-01-01 14:49:07 +0300

    exercise in string tutorial committed

---
+++ tutorial/Day-three/exercise-three.py
@@ -0,0 +1,8 @@
+# Concatenate the string 'Thirty', 'Days', 'Of', 'Python' to a single string, 'Thirty Days Of Python'.
+challenge = ['Thirty', 'Days', 'Of', 'Python']
+concat = ' ' .join(challenge)
+print(concat)
+
+#? Concatenate the string 'Coding', 'For' , 'All' to a single string, 'Coding For All'.
+quote = 'Coding' + ' ' + 'For' + ' '+ 'All'
+print(quote)
