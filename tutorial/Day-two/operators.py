96eddd23567fe490675edc34ea0bd90578abe286
Leuel-Tesfaye <131950742+Leuel-Tesfaye@users.noreply.github.com>
2024-01-01 09:52:36 +0300

    operators change committed

---
+++ tutorial/Day-two/operators.py
@@ -0,0 +1,28 @@
+
+# Assignment operators : > , < , = , != 
+
+# Arithmetic operators : + - % * 
+
+# comparison operators : 
+
+print (3 > 2)
+print( 3 >= 2)
+print(len('mango') == len('orange'))
+print(len('mango') != len('orange'))
+
+# In addition python uses the following comparison :
+#? is: Returns true if both variables are the same object(x is y)
+#? is not: Returns true if both variables are not the same object(x is not y)
+#? in: Returns True if the queried list contains a certain item(x in y)
+#? not in: Returns True if the queried list doesn't have a certain item(x in y)
+
+print('1 is 1', 1 is 1)
+print('2 is not 1', 2 is not 1)
+print('H in Hope', "H" in 'Hope')
+print('H not in Hope', "H" not in 'Hope')
+
+#! logical operators 
+print( 3 > 2 and 4 > 3)
+print (not True)
+print(not not False)
+
