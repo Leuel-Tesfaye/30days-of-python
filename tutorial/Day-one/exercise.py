dd6cc1d567d5a18c3a550bf4f852f01d046688dd
Leuel-Tesfaye <131950742+Leuel-Tesfaye@users.noreply.github.com>
2024-01-01 09:32:27 +0300

    variables change committed

---
+++ tutorial/Day-one/exercise.py
@@ -0,0 +1,18 @@
+
+#! exercise one 
+#? Declare 5 as num_one and 4 as num_two
+
+num_one , num_two = 5, 4
+total = num_one + num_two
+
+print('The sum of the two numbers is', total)
+
+division = num_one /  num_two
+print ('The subtraction of the two numbers is ', division)
+
+#? calculate the area of a circle and assign a value to a variable name of area_of_circle
+# r stands for radius 
+r = 30
+area_of_circle = 3.14 * r * r
+
+print('The area of a circle is : ', area_of_circle)
---
+++ tutorial/Day-one/variables.py
@@ -0,0 +1,16 @@
+
+# variables : refers to a memory address in which data is stored. 
+first_name = 'prince'
+lastName = 'Hope'
+country = 'Ethiopia'
+
+print(first_name)
+
+#? declaring multiple variable in one line 
+
+first_name, last_name, country, age, is_married = 'leuel', 'tesfaye', 'Ethiopia', 250, True
+
+print('First Name :' ,first_name)
+print('country :', country)
+
+
