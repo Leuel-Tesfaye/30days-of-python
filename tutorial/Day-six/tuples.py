ecbeca48e5c49014d62dc40d91ec652c83f2c47b
Leuel-Tesfaye <131950742+Leuel-Tesfaye@users.noreply.github.com>
2024-01-02 11:25:33 +0300

    tuples.py created and modified

---
+++ tutorial/Day-six/tuples.py
@@ -0,0 +1,12 @@
+
+#? Tuples : are a collection of datatypes which is ordered and immutable. 
+# once a tuple is created we can't change it's values 
+# we can't use add , insert , remove methods in tuple because it's nature is immutable.
+# This is particularly useful when a function needs to return more than one piece of information.
+        #? Example :
+        #  def get_user_info() 
+        # return username, email, age
+        # user_info = get_user_info () 
+        # username , email , age = user_info
+
+# example 2 : i might use tuples to represent fixed collection of coordinates (x,y,z)
