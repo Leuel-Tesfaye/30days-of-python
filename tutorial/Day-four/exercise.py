b413384bd3d5a99b52a7626c455c577b7aac8572
Leuel-Tesfaye <131950742+Leuel-Tesfaye@users.noreply.github.com>
2024-01-02 10:14:39 +0300

    exercising continued

---
+++ tutorial/Day-four/exercise.py
@@ -0,0 +1,16 @@
+
+# Declare a list with more than 5 items
+items = ['prince', 'Hope', '24', 'Programmer','Ethiopian', 'Ambitious']
+print(items)
+
+#? Find the length of your list
+print(len(items))
+
+#? Get the first item, the middle item and the last item of the list
+first_item = items[0]
+middle_item = items[2]
+last_item = items[-1]
+
+print('First items is :', first_item)
+print('Middle item is :', middle_item)
+print('Last Item is :' , last_item)
