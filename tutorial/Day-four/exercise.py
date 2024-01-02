38e7617245a9abac8495582e747ff9a82cc37b4f
Leuel-Tesfaye <131950742+Leuel-Tesfaye@users.noreply.github.com>
2024-01-02 10:36:14 +0300

    exercising lists inserting adding and modifying committed

--- tutorial/Day-four/exercise.py
+++ tutorial/Day-four/exercise.py
@@ -14,3 +14,23 @@ last_item = items[-1]
 print('First items is :', first_item)
 print('Middle item is :', middle_item)
 print('Last Item is :' , last_item)
+
+# Declare a list variable named it_companies and assign initial values Facebook, Google, Microsoft, Apple, IBM, Oracle and Amazon.
+IT_companies = [ 'Facebook', 'Google', 'Microsoft', 'Apple', 'Oracle' ]
+
+# Print the number of companies in the list
+print(len(IT_companies))
+
+# Print the list after modifying one of the companies
+IT_companies = [ 'Facebook', 'Google', 'Microsoft', 'Apple', 'Oracle' ]
+IT_companies [0] = 'Amazon' 
+print(IT_companies)
+
+# Add an IT company to it_companies
+IT_companies.append('IBM')
+print(IT_companies)
+
+# Insert an IT company in the middle of the companies list
+IT_companies.insert(1, 'Samsung')
+print(IT_companies)
+
