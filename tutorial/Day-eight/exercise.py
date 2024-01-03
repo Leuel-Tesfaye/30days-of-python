419adb10173876e638687b4002ee1206286b29cf
Leuel-Tesfaye <131950742+Leuel-Tesfaye@users.noreply.github.com>
2024-01-03 10:03:41 +0300

    changes made in creating and exercising dictionary ideas

--- tutorial/Day-eight/exercise.py
+++ tutorial/Day-eight/exercise.py
@@ -1,1 +1,13 @@
-print('testing')
+# Create an empty dictionary called dog
+dog = {}
+
+# Add name, color, breed, legs, age to the dog dictionary
+dog = {
+    'name' : 'Jacky',
+    'color' : 'white',
+    'breed' : 'German shepard',
+    'legs' : 'Four',
+    'age' : '5',
+}
+
+print('This are the attributes of my Dog: ', dog)
