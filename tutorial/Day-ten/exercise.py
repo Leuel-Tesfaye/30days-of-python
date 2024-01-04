54cc9a667b37b7788d4c00d92ada7a9eebdf1cd7
Leuel-Tesfaye <131950742+Leuel-Tesfaye@users.noreply.github.com>
2024-01-04 10:48:22 +0300

    countries folder and file has been created

---
+++ tutorial/Day-ten/exercise.py
@@ -0,0 +1,8 @@
+# Go to the data folder and use the countries.py file. Loop through the countries and extract all the countries containing the word land.
+
+
+for contain_land in countries :
+    if 'land' in countries:
+        print(countries)
+    else :
+           print('There is no country with the word land')
